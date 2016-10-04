9c3ab4383f6ffe62bf13f5e869da6890872223f4
Yoan Ballesteros <b.yoan@live.fr>
2016-10-04 22:59:38 +0200

    Requête sur la table comptable avec les informations fournies et vérification de la réponse

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -12,19 +12,28 @@ switch($action){
 		$login = $_REQUEST['login'];
 		$mdp = $_REQUEST['mdp'];
 		$visiteur = $pdo->getInfosVisiteur($login,$mdp);
-		if(!is_array( $visiteur)){
-			ajouterErreur("Login ou mot de passe incorrect");
-			include("vues/v_erreurs.php");
-			include("vues/v_connexion.php");
-		}
-		else{
-			$id = $visiteur['id'];
-			$nom =  $visiteur['nom'];
-			$prenom = $visiteur['prenom'];
-			connecter($id,$nom,$prenom);
-			include("vues/v_sommaire.php");
-		}
-		break;
+		$comptable = $pdo->getInfosComptable($login, $mdp);
+        
+		if(!is_array($comptable)) {
+            if (!is_array($visiteur)) {
+                ajouterErreur("Login ou mot de passe incorrect");
+                include("vues/v_erreurs.php");
+                include("vues/v_connexion.php");
+            } else {
+                $id = $visiteur['id'];
+                $nom = $visiteur['nom'];
+                $prenom = $visiteur['prenom'];
+                connecter($id, $nom, $prenom);
+                include("vues/v_sommaire.php");
+            }
+        } else {
+            $id = $comptable['id'];
+            $nom = $comptable['nom'];
+            $prenom = $comptable['prenom'];
+            connecter($id, $nom, $prenom, true);
+            include("vues/v_sommaire.php");
+        }
+        break;
 	}
 	default :{
 		include("vues/v_connexion.php");
