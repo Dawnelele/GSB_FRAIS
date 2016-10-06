bd4da4dad454152d74cee3f8141b596ccd711f7d
Yoan Ballesteros <y.ballesteros@presta-module.com>
2016-10-06 16:55:59 +0200

    Ne pas afficher les liens permettant d'accéder au formulaire de création et consultation des fiches si l'utilisateur connecté est un comptable

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -17,16 +17,15 @@
               <a href=\"index.php?uc=gererFrais&action=listeFrais\" title=\"Afficher la liste des frais\">Afficher la liste des frais</a>
            </li>" : ""?>
 
-           <li class="smenu">
+           <?php echo ($_SESSION['isComptable'] == 1) ? "" : '<li class="smenu">
               <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
            </li>
            <li class="smenu">
               <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
-           </li>
+           </li>'; ?>
            <li class="smenu">
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
            </li>
-
 
          </ul>
         
