45a7fa8ddf140c4cfbec66d50bf0006627708b6a
Yoan Ballesteros <b.yoan@live.fr>
2016-10-07 19:35:45 +0200

    Fix sql file

--- sql/gsb_frais_structure.sql
+++ sql/gsb_frais_structure.sql
@@ -53,14 +53,14 @@ CREATE TABLE IF NOT EXISTS `Etat` (
 CREATE TABLE IF NOT EXISTS `Visiteur` (
   `id` char(4) NOT NULL,
   `nom` char(30) DEFAULT NULL,
-  `prenom` char(30)  DEFAULT NULL, 
+  `prenom` char(30) DEFAULT NULL,
   `login` char(20) DEFAULT NULL,
   `mdp` char(20) DEFAULT NULL,
   `adresse` char(30) DEFAULT NULL,
   `cp` char(5) DEFAULT NULL,
   `ville` char(30) DEFAULT NULL,
   `dateEmbauche` date DEFAULT NULL,
-  `isComptable`int DEFAULT NULL,
+  `isComptable` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
